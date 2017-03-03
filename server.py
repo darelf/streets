f31cc1d79f6d9ada699f8f96efa3ea81463b620f
Darel Finkbeiner <finkbeinerd@osti.gov>
2017-03-03 15:47:34 -0500

    remove login stuff; clean up server

--- server.py
+++ server.py
@@ -1,13 +1,11 @@
 from sanic import Sanic
 from sanic.response import html, text
-from oauth2client import client, crypt
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 env = Environment(
     loader=FileSystemLoader('templates'),
     autoescape=select_autoescape(['html'])
 )
 
-CLIENT_ID = ""
 
 app = Sanic()
 
@@ -20,20 +18,4 @@ async def index(request):
     return html(env.get_template('index.html').render(title="Street Scum"))
 
 
-@app.post("/signin")
-async def signin(request):
-    token = request.form.get('token')
-    idinfo = client.verify_id_token(token, CLIENT_ID)
-    print("received info")
-
-    if idinfo['iss'] not in ['accounts.google.com', 'https://accounts.google.com']:
-        return text("invalid_issuer")
-
-    userid = idinfo['sub']
-    response = text("success")
-    response.cookies['usertoken'] = token
-    response.cookies['userid'] = userid
-
-    return response
-
-app.run("localhost", port=8080)
+app.run("0.0.0.0", port=8888)
