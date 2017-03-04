2c199a6ceca547028ddca218fe03e0768b1e0d1d
Darel Finkbeiner <darelf@me.com>
2017-03-04 08:19:34 -0500

    move stuff around, work toward better contact info

--- server.py
+++ server.py
@@ -6,6 +6,13 @@ env = Environment(
     autoescape=select_autoescape(['html'])
 )
 
+jade = {
+    'title_name': 'Jade',
+    'name': 'Jane Worth',
+    'species': 'Human',
+    'alterations': 'Skilljack',
+    'comments': ['Curabitur varius pharetra neque id porta. Duis hendrerit et massa eget consequat. In mollis diam quis lectus tempus porttitor']
+}
 
 app = Sanic()
 
@@ -13,9 +20,13 @@ app.static('/static/', './static/')
 app.static('/favicon.ico', './static/favicon.ico')
 
 
-@app.route("/")
+@app.get('/')
 async def index(request):
     return html(env.get_template('index.html').render(title="Street Scum"))
+
+@app.get('/contact/<name>')
+async def contact(request, name):
+    return html(env.get_template('contact.html').render(title="Street Scum", contact=jade))
 
 
 app.run("0.0.0.0", port=8888)
